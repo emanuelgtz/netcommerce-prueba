47150a4323c4a80091a5a51e1bafc05527aac20b
Emanuel <manudatadev@icloud.com>
2025-08-01 14:47:00 -0600

    chore(api): moved routes into api file

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\CompanyController;
-use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,8 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/company/{id}', [CompanyController::class, 'show']);
-Route::get('/company', [CompanyController::class, 'index']);
-
-Route::post('/task', [TaskController::class, 'store']);
